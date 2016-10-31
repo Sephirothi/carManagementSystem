8489b665b90f2df84efe48ac1fb8452c4d480628
dai1995910 <412966481@qq.com>
2016-10-31 21:25:43 +0800

    一不小心删了。。重新发上来

---
+++ src/test/BaseDAOTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/config/spring/spring-core.xml")
+public class BaseDAOTest extends AbstractJUnit4SpringContextTests{
+
+	
+}
