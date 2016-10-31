7e27d424fa96dcf77262e8d610e3fe41f417b2ed
47 <136698493@qq.com>
2016-10-31 20:43:34 +0800

    单元测试类更新，UserServiceImpl实现完成，Resopnse添加反射方法 注意：对查询所有数据进行测试的时候，只支持单个json对象，不支持嵌套

--- src/com/carManage/service/impl/UserServiceImpl.java
+++ src/com/carManage/service/impl/UserServiceImpl.java
@@ -1,7 +1,8 @@
 package com.carManage.service.impl;
 
-import java.util.ArrayList;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.DataFormatException;
 
 import javax.annotation.Resource;
@@ -9,6 +10,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import com.carManage.dao.BaseDAO;
+import com.carManage.dao.BaseDAO.NULL;
 import com.carManage.model.User;
 import com.carManage.service.ResponseType;
 import com.carManage.service.UserService;
@@ -17,70 +19,131 @@ import com.carManage.utils.GsonUtils;
 /**
  * 1 success 0 failure
  * 
- * @author hp
+ * @author 47
  *
  */
 @Service("userServiceImpl")
 public class UserServiceImpl extends ResponseType implements UserService {
 	@Resource(name = "userDAOImpl")
-	private BaseDAO<User> baseDao;
+	private BaseDAO<User,NULL> baseDao;
 
 	@Override
 	public String insertUser(String json) {
-		// TODO Auto-generated method stub
-		return null;
+		String result = null;
+		ReturnResponse<String> rr = new ReturnResponse<>();
+		try {
+			List<User> list = GsonUtils.jsonToList(json, User.class);
+			if(list.size()!=1){
+				//其实msg可以拿出来==!
+				result = rr.extracted(0, "传送的数据有误");
+			}else{
+				if(baseDao.insert(list.get(0))){
+					result = rr.extracted(1, "添加数据成功");
+				}else{
+					result = rr.extracted(0, "添加数据失败");
+				}
+			}
+		} catch (DataFormatException e) {
+			// 转化失败返回的结果
+			result = rr.extracted(0, "json转化失败");
+		}
+		return result;
 	}
 
 	@Override
 	public String deleteUser(String json) {
 		String result = null;
-		//返回类型
+		// 返回类型
 		ReturnResponse<String> rr = new ReturnResponse<>();
 		try {
 			List<User> list = GsonUtils.jsonToList(json, User.class);
 			if (list.size() == 1) {
-				if(baseDao.delete(list)!=1){
+				if (baseDao.delete(list) != 1) {
 					result = rr.extracted(0, "删除数据失败");
-				}else{
+				} else {
 					result = rr.extracted(1, "删除数据成功");
 				}
-				
+
 			} else {
-				result = deleteUsers(list,rr);
+				result = deleteUsers(list, rr);
 			}
 		} catch (DataFormatException e) {
 			result = rr.extracted(0, "Json转化失败");
 		}
 		return result;
 	}
-	private String deleteUsers(List<User> list,ReturnResponse<String> rr) {
+
+	private String deleteUsers(List<User> list, ReturnResponse<String> rr) {
 		String result = null;
-		if(list.size()==0){
+		if (list.size() == 0) {
 			result = rr.extracted(0, "没有选择数据");
-		}else{
-			if(baseDao.delete(list)!=list.size()){
+		} else {
+			if (baseDao.delete(list) != list.size()) {
 				result = rr.extracted(0, "删除数据失败");
-			}else{
+			} else {
 				result = rr.extracted(1, "删除数据成功");
 			}
-			
+
 		}
 		return result;
 	}
 
 	@Override
 	public String updateUser(String json) {
-		// TODO Auto-generated method stub
-		return null;
+		String result = null;
+		// 返回类型
+		ReturnResponse<String> rr = new ReturnResponse<>();
+		try {
+			List<User> list = GsonUtils.jsonToList(json, User.class);
+			if(list.size()!=1){
+				//其实msg可以拿出来==!
+				result = rr.extracted(0, "传送的数据有误");
+			}else{
+				if(baseDao.update(list.get(0))){
+					result = rr.extracted(1, "修改数据成功");
+				}else{
+					result = rr.extracted(0, "修改数据失败");
+				}
+			}
+		} catch (DataFormatException e) {
+			// 转化失败返回的结果
+			result = rr.extracted(0, "json转化失败");
+		}
+		return result;
 	}
-
 
 	@Override
 	public String queryAllUsers(String json) {
 		// TODO这个线不写,等待后台确定数据参数
-		String reslut = null;
-
-		return reslut;
+		String result = null;
+		ReturnResponse<List<User>> rr = new ReturnResponse<>();
+		
+			try {
+				Map<String, String> map = GsonUtils.jsonToMaps(json);
+				User u = getMaptoObject(map,User.class);
+				List<User> users = baseDao.query(u, 0, 0, null, null);
+				if(users==null||users.size()==0){
+					result = rr.extracted(0, "未查到符合的数据");
+				}else{
+					result = rr.extracted(1, users, baseDao.getDataCount(u));
+				}
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+				rr.extracted(0, "反射失败1");
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+				rr.extracted(0, "反射失败2");
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+				rr.extracted(0, "反射失败3");
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+				rr.extracted(0, "反射失败4");
+			} catch (DataFormatException e) {
+				rr.extracted(0, "JSON转化失败");
+			}
+		
+		return result;
 	}
 
 	@Override
@@ -91,16 +154,11 @@ public class UserServiceImpl extends ResponseType implements UserService {
 		try {
 			// 将前端传过来的数据转化为
 			List<User> user = GsonUtils.jsonToList(json, User.class);
-			// List<User> list = null;
+			System.out.println(123);
 			if (user.size() == 1) {
-				// List<User> list = baseDao.query(user.get(0));
-				List<User> list = new ArrayList<>();
-				User u = new User();
-				u.setUsername("小明");
-				u.setPassword("123");
-				list.add(u);
-				if (list.size() != 1) {
-					result = rr.extracted(0, "传输数据有误");
+				List<User> list = baseDao.query(user.get(0));
+				if (list==null||list.size() != 1) {
+					result = rr.extracted(0, "查询不到数据||传输数据有误");
 				} else {
 					result = rr.extracted(1, list.get(0), null);
 				}
@@ -133,12 +191,12 @@ public class UserServiceImpl extends ResponseType implements UserService {
 					result = rr.extracted(0, "帐号或密码错误");
 				} else {
 					// 查询数据库判断是否有这个人
-					if (baseDao.query(u).size() == 1) {
+					if (baseDao.query(u)==null||baseDao.query(u).size()!= 1) {
 						// rb.code = 1;// 表示认证成功
 						// rb.data = "登录成功";
-						result = rr.extracted(1, "登录成功");
-					} else {
 						result = rr.extracted(0, "帐号或密码错误");
+					} else {
+						result = rr.extracted(1, "登录成功");
 					}
 				}
 			} else {
--- src/test/UserServiceTest.java
+++ src/test/UserServiceTest.java
@@ -1,37 +1,43 @@
 package test;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-
-import javax.annotation.Resource;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.DataFormatException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-import com.carManage.service.UserService;
-import com.carManage.service.impl.UserServiceImpl;
+import com.carManage.model.User;
+import com.carManage.utils.GsonUtils;
 
 public class UserServiceTest {
-	
+
 	String json = "";
+
 	@Before
 	public void before() {
 		File file = new File("E:/EEout/sshCarManage/src/test/json.txt");
-		if(file.exists()){
+		if (file.exists()) {
 			FileInputStream fis = null;
 			try {
 				fis = new FileInputStream(file);
-				byte[] buff= new byte[1024];
-				int len  = 0;
-				while((len=fis.read(buff))!=-1){
-					json = json+new String(buff,0,len);
+				byte[] buff = new byte[1024];
+				int len = 0;
+				while ((len = fis.read(buff)) != -1) {
+					json = json + new String(buff, 0, len);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
-			}finally{
-				if(fis!=null){
+			} finally {
+				if (fis != null) {
 					try {
 						fis.close();
 					} catch (IOException e) {
@@ -41,11 +47,60 @@ public class UserServiceTest {
 			}
 		}
 	}
+
 	@Test
 	public void test() {
-		System.out.println(json);
-		UserService us = new UserServiceImpl();
-		String gson = us.querySingleUser(json);
-		System.out.println(gson);
+		// System.out.println(json);
+		// UserService us = new UserServiceImpl();
+		// String gson = us.querySingleUser(json);
+		// System.out.println(gson);
+		Map<String, String> map = null;
+		try {
+			map = GsonUtils.jsonToMaps(json);
+		} catch (DataFormatException e) {
+			e.printStackTrace();
+		}
+		try {
+			User u = getMaptoObject(map, User.class);
+			System.out.println(u);
+			// System.out.println(map.get("password"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 注意这里必须传输单个object对象
+	 * 
+	 * @param map
+	 * @param clazz
+	 * @return
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
+	private <T> T getMaptoObject(Map<String, String> map, Class<T> clazz)
+			throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		T t = clazz.newInstance();
+		Field[] fds = clazz.getDeclaredFields();
+		for (Field field : fds) {
+			String typename = field.getType().getTypeName();
+			String fieldname = field.getName();
+			Object obj = map.get(fieldname);
+			if (obj == null) {
+				continue;
+			}
+			if (typename.equals(String.class.getName())) {
+				PropertyDescriptor pd;
+				try {
+					pd = new PropertyDescriptor(field.getName(), clazz);
+				} catch (IntrospectionException e) {
+					continue;
+				}
+				pd.getWriteMethod().invoke(t, obj);
+			}
+		}
+		return t;
 	}
 }
