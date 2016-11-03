542a45344c3a31838301848f7e45a385324c7407
47 <136698493@qq.com>
2016-11-03 11:23:27 +0800

    useraction

--- src/com/carManage/web/action/UserAction.java
+++ src/com/carManage/web/action/UserAction.java
@@ -1,14 +1,12 @@
 package com.carManage.web.action;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletInputStream;
 
 import org.apache.struts2.ServletActionContext;
 
+import com.carManage.model.User;
 import com.carManage.service.UserService;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -19,29 +17,75 @@ public class UserAction extends ActionSupport {
 	private static final long serialVersionUID = 571641429426477845L;
 	@Resource(name = "userServiceImpl")
 	private UserService us;
-	
-	private String data ;
+
+	private String data;
+
 	public void setData(String data) {
 		this.data = data;
 	}
-	
+
 	public String getData() {
 		return data;
 	}
-	
-	
+
 	/*
-	 * 	String insertUser(String json);
-	String deleteUser(String json);
-	String updateUser(String json);
-	String queryAllUsers(String json);
-	String querySingleUser(String json);
-	String checklogin(String json);
+	 * String insertUser(String json); String deleteUser(String json); String
+	 * updateUser(String json); String queryAllUsers(String json); String
+	 * querySingleUser(String json); User checklogin(String json);
 	 */
 
 	public String login() {
-		String result = us.querySingleUser(data);
-		
+		System.out.println(data);
+		User u = us.checklogin(data);
+		if (u == null) {
+			return "error";
+		}
+		ServletActionContext.getRequest().getSession().setAttribute("user", u.getName());
 		return "success";
 	}
+
+	public String insert() {
+		String json = us.insertUser(data);
+		try {
+			ServletActionContext.getResponse().getOutputStream().print(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ERROR;
+		}
+		return "";
+	}
+
+	public String querys() {
+		String json = us.queryAllUsers(data);
+		try {
+			ServletActionContext.getResponse().getOutputStream().print(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ERROR;
+		}
+		return "";
+	}
+
+	public String query() {
+		String json = us.querySingleUser(data);
+		try {
+			ServletActionContext.getResponse().getOutputStream().print(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ERROR;
+		}
+		return "";
+	}
+
+	public String update() {
+		String json = us.updateUser(data);
+		try {
+			ServletActionContext.getResponse().getOutputStream().print(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ERROR;
+		}
+		return "";
+	}
+
 }
